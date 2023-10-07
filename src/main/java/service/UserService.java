7c76ff57b39407c3aa6215cd64c08ae6770e9611
Nanged <2074759416@qq.com>
2023-10-07 10:45:16 +0800

    Delete src/main directory

--- src/main/java/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import javaBean.User;
-
-public interface UserService {
-    User login(String username, String password);//登录
-    void register(String username, String password);//注册
-}
